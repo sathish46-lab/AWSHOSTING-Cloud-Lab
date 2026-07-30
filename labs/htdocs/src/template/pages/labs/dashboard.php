caca2591b7d2c61d48580122455216ce5dd162ad
sathishp4223 <sathishp4223@gmail.com>
2026-07-30 21:47:52 +0530

    fix: remove rounded-pill from inputs, let input-group handle rounding

--- labs/htdocs/src/template/pages/labs/dashboard.php
+++ labs/htdocs/src/template/pages/labs/dashboard.php
@@ -174,7 +174,7 @@
                             <label class="col-sm-4 col-form-label">Device IP</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="text" class="form-control rounded-pill input-device-ip" value="<?= htmlspecialchars($deviceIp) ?>">
+                                    <input readonly type="text" class="form-control input-device-ip" value="<?= htmlspecialchars($deviceIp) ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($deviceIp) ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
@@ -188,7 +188,7 @@
                             <label class="col-sm-4 col-form-label">SSH Command</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="text" class="form-control rounded-pill input-ssh-command" value="<?= htmlspecialchars($sshCommand) ?>">
+                                    <input readonly type="text" class="form-control input-ssh-command" value="<?= htmlspecialchars($sshCommand) ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($sshCommand) ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
@@ -202,7 +202,7 @@
                             <label class="col-sm-4 col-form-label">Username</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="text" class="form-control rounded-pill input-username" value="<?= htmlspecialchars($currentUsername) ?>">
+                                    <input readonly type="text" class="form-control input-username" value="<?= htmlspecialchars($currentUsername) ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($currentUsername) ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
@@ -216,7 +216,7 @@
                             <label class="col-sm-4 col-form-label">su Password</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="password" class="form-control rounded-pill input-su-password" value="<?= htmlspecialchars($sudoPass) ?>">
+                                    <input readonly type="password" class="form-control input-su-password" value="<?= htmlspecialchars($sudoPass) ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($sudoPass) ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
@@ -230,7 +230,7 @@
                             <label class="col-sm-4 col-form-label">code-server URL</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="text" class="form-control rounded-pill input-cs-url" value="<?= htmlspecialchars($creds['code_server_url'] ?? 'Not running') ?>">
+                                    <input readonly type="text" class="form-control input-cs-url" value="<?= htmlspecialchars($creds['code_server_url'] ?? 'Not running') ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($creds['code_server_url'] ?? '') ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
@@ -244,7 +244,7 @@
                             <label class="col-sm-4 col-form-label">code-server Password</label>
                             <div class="col-sm-8">
                                 <div class="input-group">
-                                    <input readonly type="password" class="form-control rounded-pill input-cs-password" value="<?= htmlspecialchars($creds['code_server_password'] ?? '') ?>">
+                                    <input readonly type="password" class="form-control input-cs-password" value="<?= htmlspecialchars($creds['code_server_password'] ?? '') ?>">
                                     <button class="btn clipboard btn-sm" data-clipboard-text="<?= htmlspecialchars($creds['code_server_password'] ?? '') ?>">
                                         <svg class="nav-icon" style="height: 15px; width: 15px;">
                                             <use xlink:href="/assets/icons/free.svg#cil-copy"></use>
